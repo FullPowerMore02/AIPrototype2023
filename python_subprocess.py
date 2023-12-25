e5c03e007b2b9b1d7eff4b0b932cac726f9c6efe
FullPowerMore02 <pongpicha.k@kkumail.com>
2023-12-25 15:44:11 +0700

    fix code

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,5 +3,5 @@ import subprocess
 if __name__ == "__main__" :
         subprocess.run(['ls','-ltr'])
         subprocess.run(['rm','-r','/home/Pongpicha/testfolder1'])
-
+        subprocess.run(['python', 'python_subprocess.py'])
         ######

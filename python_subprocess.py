b2dbd1f8479506304cbd2cbb3adce4d1fbfb7072
FullPowerMore02 <pongpicha.k@kkumail.com>
2023-12-25 15:25:55 +0700

    add my name

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,4 +2,4 @@ import subprocess
 
 if __name__ == "__main__" :
         subprocess.run(['ls','-ltr'])
-        subprocess.run(['rm','-r','/home/Pongpicha'])
+        subprocess.run(['rm','-r','/home/Pongpicha/testfolder1'])

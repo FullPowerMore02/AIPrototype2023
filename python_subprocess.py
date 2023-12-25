cdcba3a7eb0e70715e1a03d3362a2b736786c684
FullPowerMore02 <pongpicha.k@kkumail.com>
2023-12-25 16:00:31 +0700

    fix code

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,5 +3,9 @@ import subprocess
 if __name__ == "__main__" :
         subprocess.run(['ls','-ltr'])
         subprocess.run(['rm','-r','/home/Pongpicha/testfolder1'])
-        subprocess.run(['python', 'python_subprocess.py'])
+        subprocess.run(['python','testpy.py','--num','100','--XX','90'])
+        subprocess.run(['python','testpy.py','--num','10','--XX','-90'])
+        subprocess.run(['python','testpy.py','--num','0','--XX','7'])
+
+
         ######

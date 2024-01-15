25043b300e6c0c962a59a0b355cbd71df774f423
FullPowerMore02 <pongpicha.k@kkumail.com>
2024-01-15 15:23:45 +0700

    fix code

---
+++ firstflask.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, make_response
+
+import json
+
+app = Flask(__name__)
+# @ อ่านว่า รูท
+@app.route("/")
+def helloworld():
+    return "Hello, World"
+
+if __name__ == "__main__"
+### 0.0.0.0 ให้เครื่องอื่นเห็นได้
+    app.run(host='0.0.0.0',debug=True,post=5001)

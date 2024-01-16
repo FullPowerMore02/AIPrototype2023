3c12690d1ea0c865334442b58dbf838483ba186f
FullPowerMore02 <pongpicha.k@kkumail.com>
2024-01-16 10:51:10 +0700

    fix code

--- firstflask.py
+++ firstflask.py
@@ -10,13 +10,19 @@ def helloword():
 
 @app.route("/home", methods=['POST','GET'])
 def homefn():
-    print("Hello, Poro!")
-
-    namein = request.form.get('fname', '')  # เพิ่ม default value เป็น ''
-    lastnamein = request.form.get('lname', '')
-    print(namein, file=sys.stdout)
-    print(lastnamein, file=sys.stdout)
-    return render_template("home.html", name=namein)
+    if request.method == "GET":
+        print("Hello, Poro!")
+        namein = request.args.get('fname')
+        print(ืnamein,file=sys.stdout)
+        return render_template("home.html",name,namein)
+        
+    elif request.method == "POST"
+        print('we are in home(POST)',file=sys.stdout)
+        namein = request.form.get('fname', '')  # เพิ่ม default value เป็น ''
+        lastnamein = request.form.get('lname', '')
+        print(namein, file=sys.stdout)
+        print(lastnamein, file=sys.stdout)
+        return render_template("home.html", name=namein)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5001)

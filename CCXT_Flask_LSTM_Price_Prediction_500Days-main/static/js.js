6f0f15d48a076589a2c523752509e1c394d8bb23
FullPowerMore02 <113511118+FullPowerMore02@users.noreply.github.com>
2024-03-03 23:40:22 +0700

    Update js.js

--- CCXT_Flask_LSTM_Price_Prediction_500Days-main/static/js.js
+++ CCXT_Flask_LSTM_Price_Prediction_500Days-main/static/js.js
@@ -8,7 +8,7 @@ function BtnClick() { // quando apertado o botão será acionado essa função
         return;
     }
 
-    fetch(`http://127.0.0.1:5000/forecast/${num_dates}`) // faz uma solicitação GET na página
+    fetch(`http://20.198.93.92:5001/forecast/${num_dates}`) // faz uma solicitação GET na página
 
         .then(response => response.json()) // passa os dados da solicitação para JSON
         .then(data => { // pega os dados JSON e passa para a função 'data'

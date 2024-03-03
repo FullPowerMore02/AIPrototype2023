fd8f6bbba0c04ff5d7639130b5dc8793af86ee86
FullPowerMore02 <113511118+FullPowerMore02@users.noreply.github.com>
2024-03-04 00:01:26 +0700

    Update js.js

--- CCXT_Flask_LSTM_Price_Prediction_500Days-main/static/js.js
+++ CCXT_Flask_LSTM_Price_Prediction_500Days-main/static/js.js
@@ -1,40 +1,37 @@
-function BtnClick() { // quando apertado o botão será acionado essa função
+function BtnClick() {
+    const num_dates = document.getElementById("num_dates").value;
 
-    const num_dates = document.getElementById("num_dates").value; // pegar o valor de entrada na página HTML
-
-    if (isNaN(num_dates) || num_dates <= 0) { // se o input for nulo ou <= 0, ele irá retornar uma mensagem de erro na página
+    if (isNaN(num_dates) || num_dates <= 0) {
         const predictionsDiv = document.getElementById("predictions");
         predictionsDiv.innerHTML = "Add a valid number!";
         return;
     }
 
-    fetch(`http://20.198.93.92:5001/forecast/3`) // faz uma solicitação GET na página
+    fetch(`http://20.198.93.92:5001/forecast/${num_dates}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data received from server:", data);
 
-        .then(response => response.json()) // passa os dados da solicitação para JSON
-        .then(data => { // pega os dados JSON e passa para a função 'data'
-            console.log("Date received from server:", data);
-
-            const predictionsDiv = document.getElementById("predictions"); // div 'predictions' de nossa página HTML
-
+            const predictionsDiv = document.getElementById("predictions");
             let messages = [];
 
             for (const date in data.num_dates) {
-                const message = `Data: ${date} --> Expected Value: $${data.num_dates[date]}`;
+                const message = `Date: ${date}, Expected Value: $${data.num_dates[date]}`;
                 messages.push(message);
-            } // adicionando os dados do nosso forecast para uma array 'messages'
+            }
 
-            predictionsDiv.innerHTML = "Prediction Results:<br><br>" + messages.join("<br>"); // printando na tela nossas 'messages'
+            predictionsDiv.innerHTML = "Prediction Results:<br><br>" + messages.join("<br>");
 
-            const graphDiv = document.getElementById("graphDiv"); // div onde queremos exibir o gráfico
+            const graphDiv = document.getElementById("graphDiv");
+            const graphData = JSON.parse(data.graph);
 
-            const graphData = JSON.parse(data.graph); // dados do gráfico
-
-            Plotly.newPlot(graphDiv, graphData); // plotagem do gráfico
+            Plotly.newPlot(graphDiv, graphData.data, graphData.layout);
         })
         .catch(error => {
             console.error("Fetch error:", error);
-        }); // se der algum erro ele apontará no console
+        });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-});
+    // ดึงข้อมูลจาก Flask
+    const graphData = JSON.parse("{\"data\":[{\"hovertemplate\":\"index=%{x}\\u003cbr\\u003eclose=%{y}\\u003cextra\\u003e\\u003c\\u002fextra\\u003e\",\"legendgroup\":\"\",\"line\":{\"color\":\"#636efa\",\"dash\":\"solid\"},\"marker\":{\"symbol\":\"circle\"},\"mode\":\"lines\",\"name\":\"\",\"orientation\":\"v\",\"showlegend\":false,\"x\":[\"2024-03-03\",\"2024-03-04\",\"2024-03-05\",\"2024-03-06\"],\"xaxis\":\"x\",\"y\":[2917.63037109375,2904.60986328125,2875.52685546875,2837.678466796875],\"yaxis\":\"y\",\"type\":\"scatter\"}],\"layout\":{\"template\":{\"data\":{\"histogram2dcontour\":[{\"type\":\"histogram2dcontour\",\"colorbar\":{\"outlinewidth\":0,\"ticks\":\"\"},\"colorscale\":[[0.0,\"#0d0887\"],[0.1111111111111111,\"#46039f\"],[0.2222222222222222,\"#7201a8\"],[0.3333333333333333,\"#9c179e\"],[0.4444444444444444,\"#bd3786\"],[0.5555555555555556,\"#d8576b\"],[0.6666666666666666,\"#ed7953\"],[0.7777777777777778,\"#fb9f3a\"],[0.8888888888888888,\"#fdca26\"],[1.0,\"#f0f921\"]]}],\"choropleth\":[{\"type\":\"choropleth\",\"colorbar\":{\"outlinewidth\":0,\"ticks\":\"\"}}],\"histogram2d\":[{\"type\":\"histogram2d\",\"colorbar\":{\"outlinewidth\":0,\"ticks\":\"\"},\"colorscale\":[[0.0,\"#0d0887\"],[0.1111111111111111,\"#46039f\"],[0.2222222222222222,\"#7201a8\"],[0.3333333333333333,\"#9c179e\"],[0.4444444444444444,\"#bd3786\"],[0.5555555555555556,\"#d8576b\"],[0.6666666666666666,\"#ed7953\"],[0.7777777777777778,\"#fb9f3a\"],[0.8888888888888888,\"#fdca26\"],[1.0,\"#f0f921\"]]}],\"heatmap\":[{\"type\":\"heatmap\",\"colorbar\":{\"outlinewidth\":0,\"ticks\":\"\"},\"colorscale\":[[0.0,\"#0d0887\"

f2f926fd812f1c95037b0abdeda06b5e434fb222
FullPowerMore02 <113511118+FullPowerMore02@users.noreply.github.com>
2024-03-04 15:13:48 +0700

    Update js.js

--- CCXT_Flask_LSTM_Price_Prediction_500Days-main/static/js.js
+++ CCXT_Flask_LSTM_Price_Prediction_500Days-main/static/js.js
@@ -1,10 +1,10 @@
-function BtnClick() { // quando apertado o botão será acionado essa função
+function BtnClick() { 
 
-    const num_dates = 3 // pegar o valor de entrada na página HTML
-    fetch(`http://20.198.93.92:5001/forecast/${num_dates}`) // faz uma solicitação GET na página
+    const num_dates = 3 
+    fetch(`http://20.198.93.92:5001/forecast/${num_dates}`) 
 
-        .then(response => response.json()) // passa os dados da solicitação para JSON
-        .then(data => { // pega os dados JSON e passa para a função 'data'
+        .then(response => response.json()) 
+        .then(data => { 
             console.log("Date received from server:", data);
 
             let messages = [];
@@ -12,15 +12,15 @@ function BtnClick() { // quando apertado o botão será acionado essa função
             for (const date in data.num_dates) {
                 const message = `Data: ${date} --> Expected Value: $${data.num_dates[date]}`;
                 messages.push(message);
-            } // adicionando os dados do nosso forecast para uma array 'messages'
+            } 
 
-            const graphDiv = document.getElementById("graphDiv"); // div onde queremos exibir o gráfico
-            const graphData = JSON.parse(data.graph); // dados do gráfico
-            Plotly.newPlot(graphDiv, graphData); // plotagem do gráfico
+            const graphDiv = document.getElementById("graphDiv"); 
+            const graphData = JSON.parse(data.graph); // 
+            Plotly.newPlot(graphDiv, graphData); // 
         })
         .catch(error => {
             console.error("Fetch error:", error);
-        }); // se der algum erro ele apontará no console
+        }); //
 }
 
 document.addEventListener("DOMContentLoaded", function() {

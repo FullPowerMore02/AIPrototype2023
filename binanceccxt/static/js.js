23107b0bef2dda78771878168e560f497af69fcb
FullPowerMore02 <113511118+FullPowerMore02@users.noreply.github.com>
2024-03-04 15:52:15 +0700

    Update js.js

--- binanceccxt/static/js.js
+++ binanceccxt/static/js.js
@@ -1,11 +1,10 @@
-function BtnClick() {
-    const num_dates = document.getElementById("numDatesInput").value;
+function BtnClick() { 
 
-    // Validate if num_dates is a positive integer or handle validation as needed
+    const num_dates = 3 
+    fetch(`http://20.198.93.92:5001/forecast/${num_dates}`) 
 
-    fetch(`http://20.198.93.92:5001/forecast/${num_dates}`)
-        .then(response => response.json())
-        .then(data => {
+        .then(response => response.json()) 
+        .then(data => { 
             console.log("Date received from server:", data);
 
             let messages = [];
@@ -13,17 +12,16 @@ function BtnClick() {
             for (const date in data.num_dates) {
                 const message = `Data: ${date} --> Expected Value: $${data.num_dates[date]}`;
                 messages.push(message);
-            }
+            } 
 
-            const graphDiv = document.getElementById("graphDiv");
-            const graphData = JSON.parse(data.graph);
-            Plotly.newPlot(graphDiv, graphData);
+            const graphDiv = document.getElementById("graphDiv"); 
+            const graphData = JSON.parse(data.graph); // 
+            Plotly.newPlot(graphDiv, graphData); // 
         })
         .catch(error => {
             console.error("Fetch error:", error);
-        });
+        }); //
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Additional initialization if needed
+document.addEventListener("DOMContentLoaded", function() {
 });
